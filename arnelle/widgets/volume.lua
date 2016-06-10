5fd127c196cb03350375bb5318613e74f15b5173
Arnelle Balane <arnellebalane@gmail.com>
2016-06-10 15:12:34 +0800

    update volume widget to show mute text if muted

--- arnelle/widgets/volume.lua
+++ arnelle/widgets/volume.lua
@@ -7,15 +7,13 @@ volumewidgettimer:connect_signal("timeout", function()
     local status = fd:read("*all")
     fd:close()
 
-    local volume = string.match(status, "(%d?%d?%d)%%")
-    volume = string.format("% 3d", volume)
-    status = string.match(status, "%[(o[^%]])%]")
+    local volume = string.match(status, "%[(%d+%%)%]")
+    status = string.match(status, "%[(%a+)%]")
 
-    if string.find(status, "on", 1, true) then
-        volume = volume .. "%"
-    else
-        volume = volume .. "M"
+    if status == "off" then
+        volume = volume .. " (mute)"
     end
+
     volumewidget:set_text(volume)
 end)
 volumewidgettimer:start()
--- rc.lua
+++ rc.lua
@@ -204,7 +204,10 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
+    if s == 1 then
+        right_layout:add(wibox.widget.systray())
+        right_layout:add(widgetspacerspace)
+    end
     right_layout:add(volumewidget)
     right_layout:add(widgetspacerpipe)
     right_layout:add(datetimewidget)
