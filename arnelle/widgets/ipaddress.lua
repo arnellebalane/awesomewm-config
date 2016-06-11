b1ae7b538e4f55ae431a345f86c852f23b5663e8
Arnelle Balane <arnellebalane@gmail.com>
2016-06-12 03:35:16 +0800

    widget code formatting changes

--- arnelle/widgets/ipaddress.lua
+++ arnelle/widgets/ipaddress.lua
@@ -7,12 +7,14 @@ fd:close()
 
 local ip = string.match(output, "addr:(%d+%.%d+%.%d+%.%d+)")
 
+local ipwidget = wibox.widget.textbox()
+ipwidget:set_markup("<span fgcolor='" .. beautiful.ipaddress_color .. "'>" .. ip .. "</span>")
+
+
+
 local ipicon = wibox.widget.imagebox()
 ipicon:set_image(beautiful.ipaddress_icon)
 local marginedipicon = wibox.layout.margin(ipicon, 0, 5, 4, 4)
-
-local ipwidget = wibox.widget.textbox()
-ipwidget:set_markup("<span fgcolor='" .. beautiful.ipaddress_color .. "'>" .. ip .. "</span>")
 
 local iplayout = wibox.layout.fixed.horizontal()
 iplayout:add(marginedipicon)
--- arnelle/widgets/volume.lua
+++ arnelle/widgets/volume.lua
@@ -4,13 +4,11 @@ local beautiful = require("beautiful")
 
 local volumeicon = wibox.widget.imagebox()
 volumeicon:set_image(beautiful.volume_on_icon)
-local marginedvolumeicon = wibox.layout.margin(volumeicon, 0, 5, 5, 5)
 
 local volumewidget = awful.widget.progressbar({ width = 30 })
 volumewidget:set_border_color(beautiful.volume_on_color)
 volumewidget:set_color(beautiful.volume_on_color)
 volumewidget:set_background_color(beautiful.bg_normal)
-local marginedvolumewidget = wibox.layout.margin(volumewidget, 0, 0, 6, 6)
 
 local volumewidgettimer = timer({ timeout = 0.2 })
 local displayvolume = function()
@@ -37,6 +35,11 @@ end
 displayvolume()
 volumewidgettimer:connect_signal("timeout", displayvolume)
 volumewidgettimer:start()
+
+
+
+local marginedvolumeicon = wibox.layout.margin(volumeicon, 0, 5, 5, 5)
+local marginedvolumewidget = wibox.layout.margin(volumewidget, 0, 0, 6, 6)
 
 local volumelayout = wibox.layout.fixed.horizontal()
 volumelayout:add(marginedvolumeicon)
