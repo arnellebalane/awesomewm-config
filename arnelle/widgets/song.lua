f5e052e4094d86f1f67c2a41db6f69fef00c733c
Arnelle Balane <arnellebalane@gmail.com>
2017-02-06 14:11:24 +0800

    add script to get current spotify song title

--- arnelle/widgets/song.lua
+++ arnelle/widgets/song.lua
@@ -2,19 +2,8 @@ local wibox = require("wibox")
 local awful = require("awful")
 local beautiful = require("beautiful")
 
-local SP_DEST = "org.mpris.MediaPlayer2.spotify"
-local SP_PATH = "/org/mpris/MediaPlayer2"
-local SP_MEMB = "org.mpris.MediaPlayer2.Player"
-local command = "dbus-send --print-reply --session --dest=" .. SP_DEST .. " " .. SP_PATH .. " org.freedesktop.DBus.Properties.Get string:'" .. SP_MEMB .. "' string:'Metadata'"
-    .. " | grep -Ev '^method'"
-    .. " | grep -Eo '(\"(.*)\")|(\\b[0-9][a-zA-Z0-9.]*\\b)'"
-    .. " | sed -E '2~2 a|'"
-    .. " | tr -d '\\n'"
-    .. " | sed -E 's/\\|/\\n/g'"
-    .. " | sed -E 's/(xesam:)|(mpris:)//'"
-    .. " | sed -E 's/^\"|\"$//g'"
-    .. " | sed -E 's/\"+/:/g'"
-    .. " | sed -E 's/ +/ /g'"
+-- TODO: make this relative to the awesomewm install directory
+local command = "/home/arnelle/.config/awesome/arnelle/scripts/spotify"
 
 local songwidget = wibox.widget.textbox()
 local songwidgettimer = timer({ timer = 1 })
@@ -23,11 +12,7 @@ function displaysong()
     local output = fd:read("*all")
     fd:close()
 
-    local title = string.match(output, "title:(.*)%strackNumber"):gsub('&', 'and')
-    if not title then
-        title = "Open Spotify"
-    end
-    songwidget:set_markup("<span fgcolor='" .. beautiful.song_color .. "'>" .. title .. "</span>")
+    songwidget:set_markup("<span fgcolor='" .. beautiful.song_color .. "'>" .. output .. "</span>")
 end
 
 displaysong()
