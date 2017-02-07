da75dee1c926ae0af43529673998181f65f9c921
Arnelle Balane <arnellebalane@gmail.com>
2017-02-07 16:51:41 +0800

    update song.lua widget to use new features

--- arnelle/widgets/song.lua
+++ arnelle/widgets/song.lua
@@ -1,23 +1,33 @@
 local wibox = require("wibox")
 local awful = require("awful")
+local gears = require("gears")
 local beautiful = require("beautiful")
 
--- TODO: make this relative to the awesomewm install directory
-local command = "/home/arnelle/.config/awesome/arnelle/scripts/spotify"
+local command = "dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'"
+    .. "| grep -Ev '^method'"
+    .. "| grep -Eo '(\"(.*)\")|(\\b[0-9][a-zA-Z0-9.]*\\b)'"
+    .. "| sed -E '2~2 a|'"
+    .. "| tr -d '\\n'"
+    .. "| sed -E 's/\\|/\\n/g'"
+    .. "| sed -E 's/(xesam:)|(mpris:)//'"
+    .. "| sed -E 's/^\"|\"$//g'"
+    .. "| sed -E 's/\"+/:/g'"
+    .. "| sed -E 's/ +/ /g'"
+    .. "| grep 'title'"
+    .. "| sed -E 's/title://'"
 
 local songwidget = wibox.widget.textbox()
-local songwidgettimer = timer({ timer = 1 })
 function displaysong()
-    local fd = io.popen(command)
-    local output = fd:read("*all")
+    local fd = assert(io.popen(command))
+    local output = assert(fd:read("*all"))
     fd:close()
 
     songwidget:set_markup("<span fgcolor='" .. beautiful.song_color .. "'>" .. output .. "</span>")
+    return true
 end
 
 displaysong()
-songwidgettimer:connect_signal("timeout", displaysong)
-songwidgettimer:start()
+gears.timer.start_new(1, displaysong)
 
 
 
