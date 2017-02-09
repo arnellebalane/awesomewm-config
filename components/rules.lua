8f6c9f60b14ed749e502a69798e9367d45112d64
Arnelle Balane <arnellebalane@gmail.com>
2017-02-09 15:04:49 +0800

    add rules for specific client classes

--- components/rules.lua
+++ components/rules.lua
@@ -3,11 +3,12 @@
 local awful     = require("awful")
 local beautiful = require("beautiful")
 local clients   = require("components/clients")
+local tags      = require("components/tags")
 
 
 awful.rules.rules = {
     {
-        rule = {},
+        rule       = {},
         properties = {
             border_width         = beautiful.border_width,
             border_color         = beautiful.border_color,
@@ -17,6 +18,48 @@ awful.rules.rules = {
             buttons              = clients.buttons,
             maximized_horizontal = false,
             maximized_vertical   = false
-        }
+        },
+    },
+    {
+        rule       = { class = "Atom" },
+        properties = {
+            tag         = tags[1][1],
+            switchtotag = true,
+        },
+    },
+    {
+        rule       = { class = "Terminator" },
+        properties = {
+            tag         = tags[1][2],
+            switchtotag = true,
+        },
+    },
+    {
+        rule       = { class = "Hyper" },
+        properties = {
+            tag         = tags[1][2],
+            switchtotag = true,
+        },
+    },
+    {
+        rule       = { class = "Franz" },
+        properties = {
+            tag         = tags[2][2],
+            switchtotag = true,
+        },
+    },
+    {
+        rule       = { class = "Google-chrome" },
+        properties = {
+            tag         = tags[2][1],
+            switchtotag = true,
+        },
+    },
+    {
+        rule       = { class = "Nautilus" },
+        properties = {
+            tag         = tags[2][4],
+            switchtotag = true,
+        },
     }
 }
