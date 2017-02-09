4d0bd3daf5aaf4cb493225c21fab1ae557071835
Arnelle Balane <arnellebalane@gmail.com>
2017-02-09 15:04:40 +0800

    configure notifications styles

--- components/notifications.lua
+++ components/notifications.lua
@@ -6,8 +6,9 @@ local beautiful = require("beautiful")
 
 naughty.config.defaults.position     = "top_right"
 naughty.config.defaults.margin       = 10
-naughty.config.defaults.fg           = beautiful.naughty_fg or beautiful.fg_focus
-naughty.config.defaults.bg           = beautiful.naughty_bg or beautiful.bg_focus
+naughty.config.defaults.fg           = beautiful.fg_focus
+naughty.config.defaults.bg           = beautiful.bg_focus
 naughty.config.defaults.border_width = 0
 naughty.config.defaults.timeout      = 5
 naughty.config.defaults.icon_size    = 35
+naughty.config.defaults.screen       = 1
