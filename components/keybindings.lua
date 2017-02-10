f28e15a4e9969b8e20f0a29197132689715ccdc1
Arnelle Balane <arnellebalane@gmail.com>
2017-02-10 16:36:31 +0800

    add keybinding for xtrlock

--- components/keybindings.lua
+++ components/keybindings.lua
@@ -19,12 +19,15 @@ return awful.util.table.join(
     awful.key({ config.modkey            }, "Return", function() awful.spawn(config.terminal) end),
 
     -- screenshot (requires `scrot`)
-    awful.key({         }, "Print", function()
+    awful.key({                          }, "Print",  function()
         local output_path = "/home/arnelle/Pictures/screenshots/" .. os.date("%Y-%m-%d-%H%M%S" .. ".jpg")
         awful.spawn.with_shell("sleep 0.5 && scrot " .. output_path)
     end),
-    awful.key({ "Shift" }, "Print", function()
+    awful.key({ "Shift"                  }, "Print",  function()
         local output_path = "/home/arnelle/Pictures/screenshots/" .. os.date("%Y-%m-%d-%H%M%S" .. ".jpg")
         awful.spawn.with_shell("sleep 0.5 && scrot -s " .. output_path)
-    end)
+    end),
+
+    -- screen lock (requires `xtrlock`)
+    awful.key({ config.modkey            }, "F12",    function() awful.spawn("xtrlock") end)
 )
